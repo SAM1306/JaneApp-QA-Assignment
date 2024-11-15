5c086540a3144e08e4601a4b9a175b7fa0a96b4b
Saumil Jain <saumiljain@Saumils-MacBook-Pro.local>
2024-11-15 17:46:40 -0500

    Initial setup of Cypress automation framework.

--- cypress.config.js
+++ cypress.config.js
@@ -1,43 +1,49 @@
 const { defineConfig } = require("cypress")
 const fs = require("fs-extra")
 const path = require("path")
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor')
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor')
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
+const environment = process.env.ENV || 'local'
+const config = require(`./cypress/config/${environment}.js`)
 
 module.exports = defineConfig({
   projectId: "testing",
-  screenshotsFolder: 'cypress/reports/screenshots',
-  videosFolder: 'cypress/reports/videos',
+  screenshotsFolder: 'cypress/reports/assets/screenshot',
+  videosFolder: 'cypress/reports/assets/videos',
   downloadsFolder: 'cypress/downloads',
   hideXhr: true,
   viewportWidth: 1920,
   viewportHeight: 1080,
   chromeWebSecurity: false,
-  retries: {'openMode': 0, 'runMode': 1},
-  reporter: 'cypress-multi-reporters',
+  retries: {'openMode': 0, 'runMode': 0},
+  reporter: 'mochawesome',
   pageLoadTimeout: 60000,
   requestTimeout: 60000,
   defaultCommandTimeout: 10000,
   numTestKeptInMemory: 0,
   screenshotOnRunFailure: true,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin(on, config);
-      return config
-    },
+    baseUrl: config.baseURL,
+    env: config.env,
     specPattern: 'cypress/tests/**/*.feature',
-    baseUrl: 'http://localhost:3000',
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+      on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }))
+      require('cypress-mochawesome-reporter/plugin')(on)
+      return config
+    }
   },
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    reporterEnabled: 'mochawesome',
-      mochawesomeReporterOptions: {
-          useInlineDiffs: true,
-          embeddedScreenshots: true,
-          reportDir: 'cypress/reports/mocha',
-          quite: true,
-          overwrite: false,
-          html: false,
-          json: true,
-          charts: true,
-      }
+    reportDir: 'cypress/reports/mocha',
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
   }
 })
